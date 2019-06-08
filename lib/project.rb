12892b6fd66b50a1fdbc08bca1b6090550fab8b2
Becky Phan <beckyphan@live.com>
2019-06-08 23:04:38 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   
   def add_backer(backer)
     self.backers << backer
-    backer.back_projects(self) unless backer.backed_projects.include?(self)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
   
 end
